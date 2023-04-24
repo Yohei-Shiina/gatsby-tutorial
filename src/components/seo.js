b45a54d7daa83f452ee8b896dda3142eafb12a7b
Yohei Shiina <ysys.j.0125@gmail.com>
2023-04-24 08:07:51 +0000

    put back metadata

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,8 +23,7 @@ const Seo = ({ title, children }) => {
       <title>
         {seo.title} | {defaultTitle}
       </title>
-      {/* <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      <meta name="description" content={seo.description} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
@@ -34,7 +33,7 @@ const Seo = ({ title, children }) => {
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🧙</text></svg>"
-      /> */}
+      />
       {children}
     </>
   );
